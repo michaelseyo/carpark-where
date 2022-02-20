b52e6c3ea1bd676fe7e3769888f835b24da44490
michaelseyo <michael.lcw99@gmail.com>
2022-02-20 23:55:16 +0800

    Add other middlewares and error handling

--- api/routes/register.js
+++ api/routes/register.js
@@ -2,8 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', (req, res, next) => {
+    const account = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: req.body.password
+    }
     res.status(201).json({
-        message: 'Account created'
+        message: 'Account created',
+        account: account
     })
 });
 
--- app.js
+++ app.js
@@ -1,14 +1,50 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
 
 const detailRoutes = require('./api/routes/details');
 const registerRoutes = require('./api/routes/register');
 const loginRoutes = require('./api/routes/login');
 const availabilityRoutes = require('./api/routes/availability');
 
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers', 
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization"'
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
+// Routes to handle requests
 app.use('/details', detailRoutes);
 app.use('/register', registerRoutes);
 app.use('/login', loginRoutes);
 app.use('/availability', availabilityRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+});
 
 module.exports = app;
