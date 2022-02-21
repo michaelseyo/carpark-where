61aa2e845b4eed0fbc76fdffdc88f45dd6ae4141
michaelseyo <michael.lcw99@gmail.com>
2022-02-22 00:19:58 +0800

    Add delete and update acct function

--- api/routes/details.js
+++ api/routes/details.js
@@ -9,18 +9,53 @@ router.get('/', (req, res, next) => {
     });
 })
 
-router.get('/:memberId', (req, res, next) => {
+router.get('/:memberId', async (req, res, next) => {
     const id = req.params.memberId;
-    Member.findById(id)
-        .exec()
-        .then(doc => {
-            console.log("From db", doc);
+    try {
+        const doc = await Member.findById(id).exec();
+        console.log('From db', doc);
+        if (doc) {
             res.status(200).json(doc);
-        })
-        .catch(err => {
-            console.log(err)
-            res.status(500).json({error: err});
+        } else {
+            res.status(404).json({
+                message: 'No valid entry found for provided id'
+            });
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({error: err});
+    }
+});
+
+router.delete('/:memberId', async (req, res, next) => {
+    const id = req.params.memberId;
+    try {
+        result = await Member.remove({_id: id}).exec();
+        res.status(200).json(result);
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            error: err
         });
+    }
+});
+
+router.patch('/:memberId', async (req, res, next) => {
+    const id = req.params.memberId;
+    const updateOps = {};
+    // get params to update
+    for (const ops of req.body) {
+        updateOps[ops.propName] = ops.value
+    }
+
+    try {
+        result = await Member.updateOne({_id: id}, { $set: updateOps }).exec();
+        console.log(result);
+        res.status(200).json(result);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error: err})
+    }
 });
 
 module.exports = router;
--- api/routes/register.js
+++ api/routes/register.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const Member = require('../models/member');
 
-router.post('/', (req, res, next) => {
+router.post('/', async (req, res, next) => {
     const member = new Member({
         _id: new mongoose.Types.ObjectId(),
         firstName: req.body.firstName,
@@ -12,17 +12,17 @@ router.post('/', (req, res, next) => {
         email: req.body.email,
         password: req.body.password
     });
-    member
-        .save()
-        .then(result => {
-            console.log(result);
-         })
-        .catch(error => console.log(error))
-
-    res.status(201).json({
-        message: 'Account created',
-        createdMember: member
-    })
+    try {
+        result = await member.save();
+        console.log(result);
+        res.status(201).json({
+            message: 'POST request: Account created',
+            createdMember: result
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({error: err});
+    }
 });
 
 module.exports = router;
