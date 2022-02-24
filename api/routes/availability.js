7f3075a6996bdf0f2dc304913ec1574116d72d81
michaelseyo <michael.lcw99@gmail.com>
2022-02-24 14:44:16 +0800

    Add get carpark availability

--- api/routes/availability.js
+++ api/routes/availability.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
 
-router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Car park availabilites were fetched'
-    })
+const checkAuth = require('../middleware/checkAuth');
+
+router.get('/', checkAuth, async (req, res, next) => {
+    try {
+        const dataRes = await axios.get('https://api.data.gov.sg/v1/transport/carpark-availability');
+        const data = dataRes.data;
+        console.log(data);
+        res.status(200).json({
+            message: 'Current carpark availabilites were fetched',
+            data: data
+        });
+    } catch (err) {
+        console.log(err);
+    }
+
 });
 
 module.exports = router;
